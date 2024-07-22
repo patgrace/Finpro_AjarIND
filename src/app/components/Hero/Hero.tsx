1a47ccd8b4968705994d01ee34a69ed234844b39
pg.nathania@gmail.com <patricia.grace.nathania-2021@fst.unair.ac.id>
2024-07-22 18:15:07 +0700

    fix: index page

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className="bg-gradient-to-r from-purple-500 to-blue-400 p-20 text-white ">
-      <div className="max-w-4xl mx-auto text-center">
-        <h1 className="text-3xl md:text-5xl font-bold mb-4">
+    <div className="bg-gradient-to-r from-purple-500 to-blue-400 min-h-screen flex items-center justify-center text-white">
+      <div className="max-w-4xl mx-auto text-center p-18">
+        <h1 className="text-5xl font-bold mb-12">
           Bimbel Online & Offline Terbesar, Terlengkap, dan Terbukti di Indonesia
         </h1>
-        <p className="text-lg md:text-xl mb-8">
+        <p className="text-lg md:text-xl mb-12">
           Diskon spesial untukmu dengan isi nomor HP sekarang
         </p>
         <div className="flex justify-center items-center">
