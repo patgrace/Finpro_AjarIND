fe2421b537072a1a44dd0ad96e8635db1dd35f79
pg.nathania@gmail.com <patricia.grace.nathania-2021@fst.unair.ac.id>
2024-07-22 18:18:15 +0700

    fix: index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Header from '../app/components/Header/Header'
 import Hero from '../app/components/Hero/Hero'
-import { CourseListContainer } from '../app/components/Course/CourseList'
 
 export default function Page() {
   return (
     <>
     <Header />
     <Hero/>
-    <CourseListContainer/>
     </>
   )
 }
